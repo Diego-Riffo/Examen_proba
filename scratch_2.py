92fc0c0cd1f506dd9bdbcd37b9192cc14fcc97cb
Diego-Riffo <67883982+Diego-Riffo@users.noreply.github.com>
2020-07-05 22:23:43 -0400

    Update scratch_2.py

--- scratch_2.py
+++ scratch_2.py
@@ -3,10 +3,15 @@ import csv
 with open('dataSet9.csv') as File:
     reader = csv.reader(File)
     x=0
-    v[10000]
+    array = []
+
     for row in reader:
         a = float(row[0])
         x=a+x
+        array.append(a)
 
-
-    print(x/10000)
+    media=(x/10000)
+    print(media)
+    print(min(array))
+    print(max(array))
+    print(array[0])
