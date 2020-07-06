974411c8b1196906e38790df4a0846ce50a3e271
Diego-Riffo <67883982+Diego-Riffo@users.noreply.github.com>
2020-07-05 21:55:43 -0400

    Add files via upload

---
+++ scratch_2.py
@@ -0,0 +1,12 @@
+import csv
+
+with open('dataSet9.csv') as File:
+    reader = csv.reader(File)
+    x=0
+    v[10000]
+    for row in reader:
+        a = float(row[0])
+        x=a+x
+
+
+    print(x/10000)
