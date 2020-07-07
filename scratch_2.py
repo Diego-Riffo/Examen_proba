ac95d6ad94ae37fc4083d00d67f1d53099fe867e
Diego-Riffo <67883982+Diego-Riffo@users.noreply.github.com>
2020-07-07 00:30:07 -0400

    Update scratch_2.py

--- scratch_2.py
+++ scratch_2.py
@@ -3,6 +3,7 @@ import csv
 with open('dataSet9.csv') as File:
     reader = csv.reader(File)
     x=0
+    l=0
     array = []
 
     for row in reader:
@@ -12,6 +13,22 @@ with open('dataSet9.csv') as File:
 
     media=(x/10000)
     print(media)
+
+    #Minimo valor en el array
     print(min(array))
+
+    #Maximo valor en el array
     print(max(array))
-    print(array[0])
+# Probabilidad de que la siguiente llamada sea en menos de 0.5 segundos
+
+#Fuerza bruta
+    for i in range(10000):
+        if(array[i]<0.5):
+            l=l+1
+    print(l / 10000)
+
+#Exponencial
+    e = 2.718281828459045
+    exp = 1-pow(e,-(0.5/media))
+    print(exp)
+
